c01f896dd8d3923e129b38761e585a6bc77118d5
Dan Orcutt <danorc451@gmail.com>
2021-06-20 23:54:47 -0400

    Initial lobotomization of SWS

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-placeh-sys
+# Placeholder Game System
+
+A version of Atropos Simple Worldbuilding system that is as stripped down as possible.
+The goal of this system is to be the minimal possible Foundry game system that will still load successfully for testing and system development purposes only.
---
+++ module/placeh.js
@@ -0,0 +1,20 @@
+/**
+ * The minimal possible Foundry game system.
+ * Author: Norc
+ */
+
+// Import Modules
+
+
+/* -------------------------------------------- */
+/*  Foundry VTT Initialization                  */
+/* -------------------------------------------- */
+
+/**
+ * Init hook.
+ */
+Hooks.once("init", async function() {
+
+  console.log(`Initializing Placeholder System`);
+  //set CONFIGs and define any non-optional things as basically as possible here.
+});

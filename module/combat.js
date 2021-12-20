d7fc7a7f10a930cc04a1283bcaa22cb563186723
Dan Orcutt <danorc451@gmail.com>
2021-12-19 22:53:09 -0500

    added new deck sorting functionality

--- module/combat.js
+++ module/combat.js
@@ -16,15 +16,18 @@ export function addTiebreaker(combat) {
  * Catan initiative is simple, but ties are not allowed so a simple unique tiebreaker is added.
  * See Combat._getInitiativeFormula for more detail.
  * @returns {string}  Final initiative formula for the actor.
- */
+
  export const _getInitiativeFormula = function() {
     const actor = this.actor;
-    if ( !actor ) return CONFIG.Combat.initiative.formula;
+    if ( !actor ) {
+        console.log (`CATAN | No actor detected`);
+        return CONFIG.Combat.initiative.formula;
+    }
     const actorData = actor.data.data;
     const rollData = actor.getRollData();
 
     return Roll.create(formula, rollData);
-};
+}; */
 
 /**
  * Define how the array of Combatants is sorted in the displayed list of the tracker.
@@ -33,7 +36,11 @@ export function addTiebreaker(combat) {
  * Catan overrides this by adding a randomly determined unique tiebreaker to each combatant when added and sorting on that.
  * This tiebreaker value is a unique ordinal equal to or less than the total number of registered users, chosen without replacement. 
  */
+
+// TODO:    Either figure out how to actually override this function, or figure out a way to add the tiebreaker to the initiative (better).
+//          Perhaps override the roll initiative function itself?
  export const _sortCombatants = function(a, b) {
+    console.log(`CATAN | Sorting combatants`);
     const ia = Number.isNumeric(a.initiative) ? a.initiative : -9999;
     const ib = Number.isNumeric(b.initiative) ? b.initiative : -9999;
     const ci = ib - ia;
--- module/norc.js
+++ module/norc.js
@@ -20,3 +20,54 @@ export function shuffleArray(array) {
   
     return array;
   }
+
+ //should use localeCompare 
+ // caS = ca.sort((a,b) => (a.data.name > b.data.name) ? 1 : -1)
+
+ let caS = d2.cards.contents.sort((a,b) => (a.data.name > b.data.name) ? 1 : -1)
+ 
+//Replaces all cards in a deck with an array of new cards.
+//Particularly useful for sorting a deck. (replace existing cards with a sorted array)
+async function replaceEmbeddedCards(replDeck, newArr) {
+//Parameter explanations
+//replDeck is deck that needs to have its cards replaced (its Card embedded docs)
+//newArr is the array of new cards
+
+//useful contextOptions
+//deleteAll
+//keepId
+
+let delIds = getArrIds(replDeck);
+await replDeck.deleteEmbeddedDocuments("Card", delIds);
+
+let newArrData = [];
+
+newArr.forEach( (item) => newArrData.push(item.data) );
+
+await replDeck.createEmbeddedDocuments("Card", newArrData);
+return replDeck;
+}
+
+function objArrToMap(arr) {
+    let dcS = new Map();
+    arr.forEach( (item) => {
+        dcS.set(item.id, item)
+    } ); 
+    return dcS
+}
+
+d2.deleteEmbeddedDocuments({embeddedName:"Card", ids:[], context:"deleteAll"})
+
+d2.deleteEmbeddedDocuments("Card", [""], "deleteAll")
+
+d2.deleteEmbeddedDocuments("Card", [...d2.id])
+
+//Returns array of IDs of all cards in a deck
+function getArrIds(deck) {
+    let r = [];
+    deck.cards.contents.forEach( (item) => {
+        r.push(item.id);
+    })
+    return r;
+}
+
